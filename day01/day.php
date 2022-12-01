73d8b7c637aab933cd6372b88e337cf56f22f817
Grzegorz Niedzielski <grze.ndz.14@gmail.com>
2022-12-01 22:45:54 +0100

    Day 1 done

--- day01/day.php
+++ day01/day.php
@@ -2,13 +2,25 @@
 
 $input = file( __DIR__ . '/input.txt', FILE_IGNORE_NEW_LINES );
 
-
 // PART 1
+$elves = [];
+$calories = 0;
 
+foreach ( $input as $line ) {
+	if( $line !== '' ) {
+		$calories += (int) $line;
+	} else {
+		$elves[] = $calories;
+		$calories = 0;
+	}
+}
+$elves[] = $calories;
 
-echo 'Part 1: ' . '' . PHP_EOL;
+echo 'Part 1: ' . max( $elves ) . PHP_EOL;
 
 
 // PART 2
+rsort( $elves );
+$sum = array_sum( array_splice( $elves, 0, 3 ) );
 
-echo 'Part 2: ' . '' . PHP_EOL;
+echo 'Part 2: ' . $sum . PHP_EOL;
