174ac37fe822a19493126f9f733d81708d13216e
Grzegorz Niedzielski <grze.ndz.14@gmail.com>
2022-12-06 22:36:40 +0100

    Day 5 done

--- day05/day.php
+++ day05/day.php
@@ -2,13 +2,43 @@
 
 $input = file( __DIR__ . '/input.txt', FILE_IGNORE_NEW_LINES );
 
+$pos = [
+	'1' => 'SMRNWJVT',
+	'2' => 'BWDJQPCV',
+	'3' => 'BJFHDRP',
+	'4' => 'FRPBMND',
+	'5' => 'HVRPTB',
+	'6' => 'CBPT',
+	'7' => 'BJRPL',
+	'8' => 'NCSLTZBW',
+	'9' => 'LSG',
+];
+
+foreach ( $pos as $key => $stack ) {
+	$pos[ $key ] = str_split( $stack );
+}
+$pos2 = $pos1 = $pos;
+$top1 = $top2 = '';
 
 // PART 1
+foreach ( $input as $inst ) {
+	[ , $count, , $from, , $to ] = explode( ' ', $inst );
+
+	// Cut boxes to move and reverse then merge to destination
+	$move        = array_reverse( array_splice( $pos1[ $from ], - 1 * $count ) );
+	$pos1[ $to ] = array_merge( $pos1[ $to ], $move );
+
+	// For second part do not reverse the spliced part
+	$move        = array_splice( $pos2[ $from ], - 1 * $count );
+	$pos2[ $to ] = array_merge( $pos2[ $to ], $move );
+}
 
 
-echo 'Part 1: ' . '' . PHP_EOL;
 
+// Get letters from top of each stack
+foreach ( $pos1 as $stack ) $top1 .= end( $stack );
+echo 'Part 1: ' . $top1 . PHP_EOL;
 
 // PART 2
-
-echo 'Part 2: ' . '' . PHP_EOL;
+foreach ( $pos2 as $stack ) $top2 .= end( $stack );
+echo 'Part 2: ' . $top2 . PHP_EOL;
--- readme.md
+++ readme.md
@@ -7,3 +7,6 @@ The combination of array intersect and unique pretty much solved both cases, the
 
 #### Day 4 - Camp Cleanup
 Only clever bit this day was in the second part to check for pairs that do not overlap and subtract that from number of all pairs.
+
+#### Day 5 - Supply Stacks
+Crate stacks so a just a simple array without need to use something fancy. Luckily I optimized first part to use array_splice instead of loop with array_pop and second part required just not reversing the spliced part.
