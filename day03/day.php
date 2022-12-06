14468f37bc76eca01ae92e8f2ee297b6e69443c6
Grzegorz Niedzielski <grze.ndz.14@gmail.com>
2022-12-06 15:52:25 +0100

    Day 3 done

--- day03/day.php
+++ day03/day.php
@@ -2,13 +2,42 @@
 
 $input = file( __DIR__ . '/input.txt', FILE_IGNORE_NEW_LINES );
 
+function count_score( $letter ) {
+	$val = ord( $letter );
+	if ( $val < 97 ) {
+		return $val - 38;
+	}
+
+	return $val - 96;
+}
 
 // PART 1
+$score = 0;
 
+foreach ( $input as $item ) {
+	$items = str_split( $item );
+	$items = array_chunk( $items, count( $items ) / 2 );
+	$diff  = array_unique( array_intersect( $items[0], $items[1] ) );
 
-echo 'Part 1: ' . '' . PHP_EOL;
+	foreach ( $diff as $letter ) {
+		$score += count_score( $letter );
+	}
+}
+
+echo 'Part 1: ' . $score . PHP_EOL;
 
 
 // PART 2
+$score = 0;
 
-echo 'Part 2: ' . '' . PHP_EOL;
+$groups = array_chunk( $input, 3 );
+foreach ( $groups as $group ) {
+	$group = array_map( function ( $item ){
+		return str_split( $item );
+	}, $group );
+
+	$diff = array_unique( array_intersect( ...$group ) );
+	$score += count_score( array_pop( $diff ) );
+}
+
+echo 'Part 2: ' . $score . PHP_EOL;
--- readme.md
+++ readme.md
@@ -1,1 +1,6 @@
 # AoC 2022
+
+### Some highlights from this year
+
+#### Day 3 - Rucksack Reorganization
+The combination of array intersect and unique pretty much solved both cases, then it was just using ord to convert char into ASCII int and subtracting correct amount to match the goal.
