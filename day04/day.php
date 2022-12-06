09d204fd7218e258cd084896f1b13aa5149ff8f2
Grzegorz Niedzielski <grze.ndz.14@gmail.com>
2022-12-06 16:11:45 +0100

    Day 4 done

--- day04/day.php
+++ day04/day.php
@@ -2,13 +2,43 @@
 
 $input = file( __DIR__ . '/input.txt', FILE_IGNORE_NEW_LINES );
 
+function parse_pair( $item ) {
+	$pair = explode( ',', $item );
+
+	return array_map( function ( $item ) {
+		return explode( '-', $item );
+	}, $pair );
+}
 
 // PART 1
+$score = 0;
 
+// Check each pair that contains one another fully in the range
+foreach ( $input as $item ) {
+	$pair = parse_pair( $item );
 
-echo 'Part 1: ' . '' . PHP_EOL;
+	if ( $pair[0][0] <= $pair[1][0] && $pair[0][1] >= $pair[1][1] ) {
+		$score ++;
+		continue;
+	}
+	if ( $pair[0][0] >= $pair[1][0] && $pair[0][1] <= $pair[1][1] ) {
+		$score ++;
+	}
+}
+
+echo 'Part 1: ' . $score . PHP_EOL;
 
 
 // PART 2
+$score = count( $input );
 
-echo 'Part 2: ' . '' . PHP_EOL;
+// Subtract pairs that do not overlap at all from total number of pairs
+foreach ( $input as $item ) {
+	$pair = parse_pair( $item );
+
+	if ( $pair[0][1] < $pair[1][0] || $pair[0][0] > $pair[1][1] ) {
+		$score --;
+	}
+}
+
+echo 'Part 2: ' . $score . PHP_EOL;
--- readme.md
+++ readme.md
@@ -4,3 +4,6 @@
 
 #### Day 3 - Rucksack Reorganization
 The combination of array intersect and unique pretty much solved both cases, then it was just using ord to convert char into ASCII int and subtracting correct amount to match the goal.
+
+#### Day 4 - Camp Cleanup
+Only clever bit this day was in the second part to check for pairs that do not overlap and subtract that from number of all pairs.
