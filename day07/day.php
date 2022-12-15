cc5c6731fa6981f0838dc77ea78ffe05dfc003a2
Grzegorz Niedzielski <grze.ndz.14@gmail.com>
2022-12-15 02:40:36 +0100

    Day 7 done

--- day07/day.php
+++ day07/day.php
@@ -4,11 +4,93 @@ $input = file( __DIR__ . '/input.txt', FILE_IGNORE_NEW_LINES );
 
 
 // PART 1
+$pwd   = [];
+$drive = [];
 
+// Parse directories
+foreach ( $input as $line ) {
 
-echo 'Part 1: ' . '' . PHP_EOL;
+	if( $line === '$ ls' ) continue;
+
+	if( $line === '$ cd /' ) {
+		$pwd = [ '.' ];
+		continue;
+	}
+
+	if( $line === '$ cd ..' ) {
+		array_pop( $pwd );
+		continue;
+	}
+
+	if( str_starts_with( $line, '$ cd ' ) ) {
+		$pwd[] = str_replace( '$ cd ', '', $line );
+		continue;
+	}
+
+	// File
+	$cmd = explode( ' ', $line );
+	if ( is_numeric( $cmd[0] ) ) {
+		$dir = implode( '/', $pwd );
+
+		$drive[ $dir ] ??= [
+			'files' => [],
+			'size'  => 0,
+		];
+
+		if ( ! in_array( $cmd[1], $drive[ $dir ]['files'] ) ) {
+			$drive[ $dir ]['files'][] = $cmd[1];
+			$drive[ $dir ]['size']    += (int) $cmd[0];
+		}
+	}
+}
+
+// Parse folder sizes to include children
+foreach ( $drive as $name => $dir ) {
+	foreach ( $drive as $name2 => $dir2 ) {
+		if ( $name !== $name2 && strpos( $name2, $name ) === 0 ) {
+			$drive[ $name ]['size'] += $drive[ $name2 ]['size'];
+		}
+	}
+}
+
+// Create missing directories
+// If there is dir ./a/b/c and dir b only contains dir c
+// it needs to be added to drive
+foreach ( $drive as $name => $dir ) {
+	$partial = explode( '/', $name );
+
+	while( count( $partial ) > 1 ) {
+		$prev = $drive[ implode( '/', $partial ) ]['size'];
+		array_pop( $partial );
+
+		$drive[ implode( '/', $partial ) ] ??= [
+			'files' => [],
+			'size'  => $prev,
+		];
+	}
+}
+
+// Calculate size of all dirs under 10K
+$size = 0;
+foreach ( $drive as $dir ) {
+	if ( $dir['size'] <= 100000 ) {
+		$size += $dir['size'];
+	}
+}
+echo 'Part 1: ' . $size . PHP_EOL;
 
 
 // PART 2
+$limit = abs( 40000000 - $drive['.']['size'] );
+$gap = PHP_INT_MAX;
 
-echo 'Part 2: ' . '' . PHP_EOL;
+// Check smallest directory that is bigger than limit we need to pass
+foreach ( $drive as $dir ) {
+	$diff = $dir[ 'size' ] - $limit;
+	if( $diff > 0 && $diff < $gap ) {
+		$gap = $diff;
+		$target = $dir['size'];
+	}
+}
+
+echo 'Part 2: ' . $target . PHP_EOL;
--- readme.md
+++ readme.md
@@ -13,3 +13,8 @@ Crate stacks so a just a simple array without need to use something fancy. Lucki
 
 #### Day 6 - Tuning Trouble
 Instead of going for any type of array I kept all processing in first part as a string using count_chars with mode 3 that returns string of unique characters if its length was 4 it meant all 4 were unique. It was perfect for the second part with minimal alternations.
+
+#### Day 7 - No Space Left On Device
+The obvious solution would be to create tree, but I went with an associative array in which key was directory path. The traversing was very simple as I used array to keep current directory path adding to the end or popping last element to go level up.
+But then I needed to update size of each directory to contain its parent, and that again was done without recursive tree traversing but with double loop to check every directory against each other checking if the path contains one another i.e. path ./a should contain size of child directory ./a/b/c - all of that with simple strpos.
+Last tricky bit was adding missing directories that have not been initially in array of paths as task required to take any directory into considreation.
